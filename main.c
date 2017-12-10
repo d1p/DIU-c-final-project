84f424e290d46dd405f5f5567cfe761fd52f4a80
Nihan Dip <nihan.dip@gmail.com>
2017-12-10 21:14:47 +0600

    final

--- main.c
+++ main.c
@@ -4,20 +4,17 @@
 #include <time.h>
 #include <unistd.h>
 #include <sys/resource.h>
-#include <sys/time.h>
+#include <time.h>
 #include <string.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <netdb.h> 
 
-#undef calculate
-#undef getrusage
-
 #define MIN_BOARD_SIZE 3
 #define MAX_BOARD_SIZE 9
-#define SERVER_ADDRESS "www.example.com"
+#define SERVER_ADDRESS "lit-harbor-28534.herokuapp.com"
 
-char id[11];
+char id[30];
 
 int board[MIN_BOARD_SIZE][MIN_BOARD_SIZE];
 int dimension;
@@ -31,11 +28,14 @@ int move(int tile);
 int won(void);
 int getInt(void);
 int getLvl(void);
-double calculate(const struct rusage* b, const struct rusage* a);
 void error(char a[100]);
 
 int main(void)
 {
+    // First remove the log file if that exists.
+    remove("log.txt");
+
+    // Now open the log file to record all the moves.
     FILE *file = fopen("log.txt", "w");
     if (file == NULL)
     {
@@ -45,15 +45,14 @@ int main(void)
     greet();
 
     // structs for timing data
-    struct rusage before, after;
+    time_t start_t, end_t;
     double time_taken = 0.0;
-    getrusage(RUSAGE_SELF, &before);
+    time(&start_t);
 
     init();
 
     while (1 == 1)
     {
-        break;
         clear();
         draw();
         for (int i = 0; i < dimension; i++)
@@ -78,13 +77,11 @@ int main(void)
         printf("Tile to move: ");
         int tile = getInt();
 
-        // Quit the game if the user enters 0 (TEST)
         if (move(tile) == 0)
         {
             printf("Illegal move.\n");
             usleep(500000);
         }
-
         // Logging for testing purpose.
         fprintf(file, "%i\n", tile);
         fflush(file);
@@ -97,19 +94,18 @@ int main(void)
     }
     usleep(400000);
     fclose(file);
-    getrusage(RUSAGE_SELF, &after);
-    time_taken = calculate(&before, &after);
+    time(&end_t);
+    time_taken = difftime(end_t, start_t);
 
-    int port_number = 443;
+    int port_number = 80;
     char *host = SERVER_ADDRESS;
-    char *message_fmt = "POST /id=%s&time=%lf HTTP/1.0\r\n\r\n";
-
+    char *message_fmt = "GET /submit/?id=%s&time=%lf&level=%i HTTP/1.0\r\nHost: lit-harbor-28534.herokuapp.com\r\n\r\n";
     struct hostent *server;
     struct sockaddr_in serv_addr;
     int sockfd, bytes, sent, received, total;
     char message[1024], response[4096];
-    sprintf(message, message_fmt, id, time_taken);
-    printf("Request:\n%s\n",message);
+    sprintf(message, message_fmt, id, time_taken, dimension);
+    //printf("Request:\n%s\n",message);
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if (sockfd < 0)
     {
@@ -127,7 +123,7 @@ int main(void)
 
     /* connect the socket */
     if (connect(sockfd,(struct sockaddr *)&serv_addr,sizeof(serv_addr)) < 0)
-        error("ERROR connecting");
+        error("ERROR Connecting to the internet");
 
     /* send the request */
     total = strlen(message);
@@ -135,7 +131,7 @@ int main(void)
     do {
         bytes = write(sockfd,message+sent,total-sent);
         if (bytes < 0)
-            error("ERROR writing message to socket");
+            error("ERROR Connecting to the internet");
         if (bytes == 0)
             break;
         sent+=bytes;
@@ -148,21 +144,21 @@ int main(void)
     do {
         bytes = read(sockfd,response+received,total-received);
         if (bytes < 0)
-            error("ERROR reading response from socket");
+            error("ERROR! Score saving failed.");
         if (bytes == 0)
             break;
         received+=bytes;
     } while (received < total);
 
     if (received == total)
-        error("ERROR storing complete response from socket");
+        error("ERROR! Score saving failed.");
 
     /* close the socket */
     close(sockfd);
 
     /* process response testing purpose*/
-    printf("Response:\n%s\n",response);
-
+    //printf("Response:\n%s\n",response);
+    printf("Score saved. Check your score at https://lit-harbor-28534.herokuapp.com/\n");
     return 0;
 }
 
@@ -183,7 +179,7 @@ void clear(void)
 void greet(void)
 {
     clear();
-    printf("Welcome to Puzzle. Please enter your student ID: ");
+    printf("Welcome to Puzzle. Please enter your name: ");
     scanf("%[^\n]%*c", id);
     dimension = getLvl();
     usleep(1000000);
@@ -202,6 +198,8 @@ int getLvl(void)
 void init(void)
 {
     // Calculate number of slots needed.
+    // We need to remove 1 from the board because 
+    //we need the place where user can move the tile.
     int tiles_number = (dimension * dimension) - 1;
 
     // This checks if the board is even or odd. Using shorthand if else.
@@ -253,6 +251,7 @@ void draw(void)
 
 int won(void)
 {
+    // Basically we have to check if board[0][0] == 1, board[0][1] == 2 and on and on.
     int win_tiles = 1;
     for (int i = 0; i < dimension; i++)
     {
@@ -315,31 +314,11 @@ int move(int tile)
                 {
                     return 0;
                 }
-                // return true after a valid/legal move
                 return 1;
             }
         }
     }
     return 0;
-}
-
-/**
- * Returns number of seconds between b and a.
- */
-double calculate(const struct rusage* b, const struct rusage* a)
-{
-    if (b == NULL || a == NULL)
-    {
-        return 0.0;
-    }
-    else
-    {
-        return ((((a->ru_utime.tv_sec * 1000000 + a->ru_utime.tv_usec) -
-                 (b->ru_utime.tv_sec * 1000000 + b->ru_utime.tv_usec)) +
-                ((a->ru_stime.tv_sec * 1000000 + a->ru_stime.tv_usec) -
-                 (b->ru_stime.tv_sec * 1000000 + b->ru_stime.tv_usec)))
-                / 1000000.0);
-    }
 }
 
 void error(char a[100])
